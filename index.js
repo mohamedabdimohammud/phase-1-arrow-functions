391d6dcb04ef78375b6baa87c020a79f9900d402
mohamedabdimohammud <mmohammud@outlook.com>
2023-03-14 08:40:21 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+// Define the divide function expression
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Define the square arrow function
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Define the add arrow function
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+  
